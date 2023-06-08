7ddd90c760da6509fda3d7e0ce1c795b6e68411c
Vamsi <vamsi.ampolu@complispace.com.au>
2023-06-08 15:36:08 +1000

    use linaria to style the game.

--- src/Moves.js
+++ src/Moves.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { styled } from "@linaria/react";
+
+const MoveButton = styled.button`
+  min-width: 120px;
+`;
+
+const MoveListItem = styled.li`
+  margin-top: 10px;
+`;
+
 export function Moves({ history, jumpToMove }) {
   const moves = history.map((_, index) => (
-    <li key={index}>
+    <MoveListItem key={index}>
       <Move number={index} onClick={() => jumpToMove(index)} />
-    </li>
+    </MoveListItem>
   ));
 
   return <ol>{moves}</ol>;
@@ -15,5 +25,5 @@ export function Move({ number, onClick }) {
     description = `Go to move ${number}.`;
   }
 
-  return <button onClick={onClick}>{description}</button>;
+  return <MoveButton onClick={onClick}>{description}</MoveButton>;
 }
